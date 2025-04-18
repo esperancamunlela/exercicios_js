954a851c804105985154399c2f3083d04a0a9460
esperancamunlela <esperancamunlela@gmail.com>
2025-04-18 23:36:55 +0200

    strings

--- s9.js
+++ s9.js
@@ -1,5 +1,6 @@
 /*9. Encontre a posição da primeira ocorrência de uma palavra dentro de uma string
 usando .indexOf().*/
-const frase = "sou uma bela mulher, muito bela";
+let frase = "sou uma bela mulher, muito bela";
+frase = frase.split(" ");
 
-console.log(frase.indexOf("bela"));
+console.log(frase.indexOf("bela")+1);
